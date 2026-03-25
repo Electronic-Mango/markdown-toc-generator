30a460646b548244db9c06c0175620d227690164
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-03-26 00:00:46 +0100

    Move some logic back to file_toc.py
    Less cluttered full_toc.py, everything in one place to print summary ToC.

--- src/file_toc.py
+++ src/file_toc.py
@@ -2,7 +2,33 @@ from os import linesep
 from pathlib import Path
 from re import MULTILINE, sub
 
+from header import Header
+
 TOC_REGEX = r"^(#[^#].+)$(\s*-.+\n)*\s*"
+
+
+def handle_file_toc(
+    header_data: dict[Path, list[Header]], skip: int, take: int, in_place: bool
+) -> None:
+    for path, headers in header_data.items():
+        if not (toc := format_headers(headers, skip, take, True)):
+            continue
+        if in_place:
+            insert_toc(path, toc)
+        else:
+            print(f"{path}:{linesep}{toc}{linesep}")
+
+
+def format_headers(headers: list[Header], skip: int, take: int, section_only: bool) -> str:
+    return linesep.join(
+        header.str(skip, section_only)
+        for header in headers
+        if level_in_range(header.level, skip, take)
+    )
+
+
+def level_in_range(level: int, skip: int, take: int) -> bool:
+    return (level > skip and level <= (take + skip)) if take else (level > skip)
 
 
 def insert_toc(path: Path, toc: str) -> None:
--- src/full_toc.py
+++ src/full_toc.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser, Namespace
-from os import linesep
 from pathlib import Path
 
-from file_toc import insert_toc
+from file_toc import handle_file_toc
 from header import Header
 from parse_headers import parse_headers_from_file
 
@@ -15,8 +14,8 @@ def main():
     normalized_excludes = {normalize(root, path) for path in args.exclude}
     notes_paths = get_all_notes_paths(root, normalized_excludes)
     notes_paths.sort(key=lambda path: (len(path.parents), path))
-    header_data = parse_headers_from_all_notes(notes_paths)
-    update_toc_in_files(header_data, args.skip, args.take, args.in_place)
+    header_data = parse_all_headers(notes_paths)
+    handle_file_toc(header_data, args.skip, args.take, args.in_place)
 
 
 def parse_arguments() -> Namespace:
@@ -45,32 +44,8 @@ def check_excluded_path(path: Path, excluded: Path) -> bool:
     return path == excluded if excluded.is_file() else excluded in path.parents
 
 
-def parse_headers_from_all_notes(notes_paths: list[Path]) -> dict[Path, list[Header]]:
+def parse_all_headers(notes_paths: list[Path]) -> dict[Path, list[Header]]:
     return {path: parse_headers_from_file(path) for path in notes_paths}
-
-
-def update_toc_in_files(
-    header_data: dict[Path, list[Header]], skip: int, take: int, in_place: bool
-) -> None:
-    for path, headers in header_data.items():
-        if not (toc := format_headers(headers, skip, take, True)):
-            continue
-        if in_place:
-            insert_toc(path, toc)
-        else:
-            print(f"{path}:{linesep}{toc}{linesep}")
-
-
-def format_headers(headers: list[Header], skip: int, take: int, section_only: bool) -> str:
-    return linesep.join(
-        header.str(skip, section_only)
-        for header in headers
-        if level_in_range(header.level, skip, take)
-    )
-
-
-def level_in_range(level: int, skip: int, take: int) -> bool:
-    return (level > skip and level <= (take + skip)) if take else (level > skip)
 
 
 if __name__ == "__main__":
